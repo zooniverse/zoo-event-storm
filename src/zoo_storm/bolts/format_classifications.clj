8d879a32fc7ba1e36e60263d0be3a5b981e51a9b
Edward Paget <ed.paget@gmail.com>
2014-04-25 10:18:12 -0500

    Adjust formatting

--- src/zoo_storm/bolts/format_classifications.clj
+++ src/zoo_storm/bolts/format_classifications.clj
@@ -6,21 +6,30 @@
             [clojure.java.io :refer :all])
   (:gen-class))
 
+(defn get-ans-key
+  [ans k]
+  (-> (filter #(contains? % k) ans) first k))
+
 (defbolt format-classifications ["event" "type" "project"] {:params [projects]} [tuple collector]
   (let [cls (tuple "classification")
         ans (:annotations cls)  
         form (formatters :date-time-no-ms)
         t (str/replace (str/replace (:timestamp cls) #"\s(UTC|GMT)" "Z")
-                       #"\s" "T")]
+                       #"\s" "T")
+        user-agent (or (get-ans-key ans :agent)
+                       (get-ans-key ans :user_agent))
+        data (filter #(not (or (contains? % :lang) 
+                               (contains? % :agent)
+                               (contains? % :user_agent))) 
+                     ans)]
     (when (some #{(:project cls)} projects)
       (emit-bolt! collector [{:user_id (or (:user_id cls) "Not Logged In")
                               :user_ip (:user_ip cls)
-                              :subjects (str/join "," (:zooniverse_id (:subject cls)))
-                              :lang (-> (filter #(contains? % :lang) ans) first
-                                        :lang)
-                              :user_agent (-> (filter #(contains? % :agent) ans) first :agent)
+                              :subjects (str/join "," (map :zooniverse_id (:subject cls)))
+                              :lang (get-ans-key ans :lang)
+                              :user_agent user-agent
                               :user_name (or (:user cls) "Not Logged In")
-                              :data (filter #(not (or (contains? % :lang) (contains? % :agent))) ans)
+                              :data data
                               :created_at (parse form t)}
                              "classifications"
                              (:project cls)]
